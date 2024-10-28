4fd9545f2094a3dae851196f6580bd6d1bb63df1
hugo.prunaux <hugo.prunaux@gmail.com>
2024-10-28 18:23:00 +0100

    fix: handles array elements having objects

--- __tests__/MongoBulkDataMigration.rollback.test.ts
+++ __tests__/MongoBulkDataMigration.rollback.test.ts
@@ -412,6 +412,29 @@ describe('MongoBulkDataMigration', () => {
         expect(restoredDocuments).toEqual([document]);
       });
 
+      it('should restore an array element having objects', async () => {
+        const document = {
+          array: [
+            {
+              nested: [
+                "nestedValue"
+              ]
+            }
+          ]
+        }
+        await collection.insertMany([document]);
+        const dataMigration = new MongoBulkDataMigration({
+          ...DM_DEFAULT_SETUP,
+          update: { $set: { array: [] } },
+        });
+
+        await dataMigration.update();
+        await dataMigration.rollback();
+
+        const restoredDocuments = await collection.find().toArray();
+        expect(restoredDocuments).toEqual([document]);
+      });
+
       it('should not restore non-projected sibling values', async () => {
         const { insertedIds } = await collection.insertMany([sampleDocument]);
         const dataMigration = new MongoBulkDataMigration({
