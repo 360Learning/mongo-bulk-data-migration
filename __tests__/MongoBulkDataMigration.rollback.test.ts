680e9e2d650b7af4f295490991178639b1e6678f
Patrick Portal <patrick.portal@360learning.com>
2024-10-31 16:51:03 +0100

    test: add coverage for setting back value after  an `$unset`  (#20)
    * test: add coverage for  in rollback spec
    
    * test: ensure  works in nested paths

--- __tests__/MongoBulkDataMigration.rollback.test.ts
+++ __tests__/MongoBulkDataMigration.rollback.test.ts
@@ -369,6 +369,90 @@ describe('MongoBulkDataMigration', () => {
       });
     });
 
+    describe('$unset support', () => {
+      it('should restore an object values', async () => {
+        const document = { key: 'value' };
+        await collection.insertMany([document]);
+        const dataMigration = new MongoBulkDataMigration({
+          ...DM_DEFAULT_SETUP,
+          update: { $unset: { key: 1 } },
+          projection: { key: 1 },
+        });
+
+        await dataMigration.update();
+        await dataMigration.rollback();
+
+        const restoredDocuments = await collection.find().toArray();
+
+        expect(restoredDocuments).toEqual([document]);
+      });
+
+      it('should restore an object values even if key is not projected', async () => {
+        const document = { key: 'value' };
+        await collection.insertMany([document]);
+        const dataMigration = new MongoBulkDataMigration({
+          ...DM_DEFAULT_SETUP,
+          update: { $unset: { key: 1 } },
+        });
+
+        await dataMigration.update();
+        await dataMigration.rollback();
+
+        const restoredDocuments = await collection.find().toArray();
+
+        expect(restoredDocuments).toEqual([document]);
+      });
+
+      it('should restore a nested object value', async () => {
+        const document = { deep: { key: 'value' } };
+        await collection.insertMany([document]);
+        const dataMigration = new MongoBulkDataMigration({
+          ...DM_DEFAULT_SETUP,
+          update: { $unset: { 'deep.key': 1 } },
+          projection: { deep: 1 },
+        });
+
+        await dataMigration.update();
+        await dataMigration.rollback();
+
+        const restoredDocuments = await collection.find().toArray();
+
+        expect(restoredDocuments).toEqual([document]);
+      });
+
+      it('should restore a nested object value even if key is not projected', async () => {
+        const document = { deep: { key: 'value' } };
+        await collection.insertMany([document]);
+        const dataMigration = new MongoBulkDataMigration({
+          ...DM_DEFAULT_SETUP,
+          update: { $unset: { 'deep.key': 1 } },
+        });
+
+        await dataMigration.update();
+        await dataMigration.rollback();
+
+        const restoredDocuments = await collection.find().toArray();
+
+        expect(restoredDocuments).toEqual([document]);
+      });
+
+      it('should restore a nested object value in array', async () => {
+        const document = { deep: { key: { 0: 'value' } } };
+        await collection.insertMany([document]);
+        const dataMigration = new MongoBulkDataMigration({
+          ...DM_DEFAULT_SETUP,
+          update: { $unset: { 'deep.key.0': 1 } },
+        });
+
+        await dataMigration.update();
+        await dataMigration.rollback();
+
+        const restoredDocuments = await collection.find().toArray();
+
+        expect(restoredDocuments).toEqual([document]);
+      });
+    });
+
     describe('Number indexed objects support', () => {
       it('should restore an object with numbers as keys', async () => {
         const document = {
